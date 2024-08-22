1afbd5e8ec01786fe7da5f7454990953bc96b794
정승우 <ssounsowns@gmail.com>
2024-08-22 22:26:19 +0900

    팔로우 추천 탭 창

---
+++ instaClone/src/components/suggestTab.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const SuggestTab = () => {
+  return <Wrapper></Wrapper>;
+};
+export default SuggestTab;
+const Wrapper = styled.div`
+  border: solid black 1px;
+  margin-right: 70px;
+  margin-top: 40px;
+  width: 300px;
+  height: 500px;
+`;
