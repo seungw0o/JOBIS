1205e2f49b8bd19a353f916acac9e07bd8d5e969
정승우 <ssounsowns@gmail.com>
2024-08-22 19:45:17 +0900

    글로벌 스타일 추가

--- instaClone/src/App.jsx
+++ instaClone/src/App.jsx
@@ -1,5 +1,30 @@
+import styled, { createGlobalStyle } from "styled-components";
+import Home from "./pages/home";
+import reset from "styled-reset";
+
 function App() {
-  return <></>;
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      <Home />
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing:border-box;
+  }
+  body {
+  background-color: white;
+  }
+`;
