0ea0ecd2da6a239770bf1692e9de1fa639431741
정승우 <ssounsowns@gmail.com>
2024-08-25 22:56:39 +0900

    홈에 피드를 추가

--- instaClone/src/pages/home.jsx
+++ instaClone/src/pages/home.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import StoryBoard from "../components/storyBoard";
 import SideMenu from "../components/sideMenu";
 import SuggestTab from "../components/suggestTab";
+import Peed from "../components/peed";
+import PeedBoard from "../components/peedBoard";
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
       <SideMenu />
       <Container>
         <StoryBoard />
+        <PeedBoard />
       </Container>
       <RightSide>
         <SuggestTab />
@@ -22,11 +25,13 @@ export default Home;
 const RightSide = styled.div`
   display: flex;
   padding: 0px 0px 0px 9px;
+  box-sizing: border-box;
 `;
 
 const Container = styled.div`
   display: flex;
   margin-left: 350px;
+  flex-direction: column;
 `;
 
 const Wrapper = styled.div`
