de2ac12df1f3bbb923288640d6dde8ee05971794
정승우 <ssounsowns@gmail.com>
2024-08-27 08:37:01 +0900

    마진 주석 해제

--- instaClone/src/components/suggestTab.jsx
+++ instaClone/src/components/suggestTab.jsx
@@ -6,7 +6,7 @@ const SuggestTab = () => {
 export default SuggestTab;
 const Wrapper = styled.div`
   border: solid black 1px;
-  /* margin-right: 70px; */
+  margin-right: 70px;
   margin-top: 40px;
   width: 300px;
   height: 500px;
