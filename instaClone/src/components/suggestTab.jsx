14aefb8bc884c6766589fcd40f21f83ef33761ac
정승우 <ssounsowns@gmail.com>
2024-08-28 18:58:24 +0900

    팔로우 추천바 수정
    마진값 삭제, wrapper에 아무것도 없음으로 하나로 변경

--- instaClone/src/components/suggestTab.jsx
+++ instaClone/src/components/suggestTab.jsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 const SuggestTab = () => {
-  return <Wrapper></Wrapper>;
+  return <Wrapper />;
 };
 export default SuggestTab;
 const Wrapper = styled.div`
   border: solid black 1px;
-  margin-right: 70px;
+
   margin-top: 40px;
   width: 300px;
   height: 500px;
