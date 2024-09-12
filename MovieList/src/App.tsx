07456687ac49a7c85b4d5ab8b2af3a48506fe70c
정승우 <ssounsowns@gmail.com>
2024-09-12 23:51:32 +0900

    router , globalstyle set

--- MovieList/src/App.tsx
+++ MovieList/src/App.tsx
@@ -1,5 +1,30 @@
+import styled, { createGlobalStyle } from "styled-components";
+import MainRouter from "./routers/MainRouter";
+import reset from "styled-reset";
+
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      <MainRouter />
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing:border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+  background-color: black;
+  }
+`;
