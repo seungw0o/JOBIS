6346a4bc20703d66bdb82179b73b441728915f2c
정승우 <ssounsowns@gmail.com>
2024-09-18 01:51:21 +0900

    영화 구현
    영화 포스터, 제목, 설명 등이 담겨있음

---
+++ MovieList/src/components/Movie.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+interface MovieProps {
+  title: string;
+  coverimg: string;
+  summary: string;
+}
+
+const MovieScreen = ({ title, coverimg, summary }: MovieProps): JSX.Element => {
+  return (
+    <Wrapper>
+      <CoverImg src={coverimg} alt={title} />
+      <MovieTitle>{title}</MovieTitle>
+      <MovieSummary>{summary}</MovieSummary>
+    </Wrapper>
+  );
+};
+
+export default MovieScreen;
+
+const MovieSummary = styled.p`
+  float: right;
+  color: black;
+`;
+
+const Wrapper = styled.div`
+  background-color: white;
+  margin-bottom: 70px;
+  font-weight: 300;
+  padding: 20px;
+  border-radius: 5px;
+  color: #adaeb9;
+  display: grid;
+  grid-template-columns: minmax(150px, 1fr) 2fr;
+  grid-gap: 20px;
+  text-decoration: none;
+  color: inherit;
+  box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
+    0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6px 16px -6px rgba(0, 0, 0, 0.025);
+`;
+const CoverImg = styled.img`
+  position: relative;
+  top: -50px;
+  max-width: 150px;
+  height: 200px;
+  width: 100%;
+  margin-right: 30px;
+  box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
+    0 18px 36px -18px rgba(0, 0, 0, 0.3), 0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+`;
+const MovieTitle = styled.h2`
+  margin: 0;
+  font-weight: 300;
+  text-decoration: none;
+  color: black;
+  & > a {
+    margin-bottom: 5px;
+    font-size: 24px;
+    color: #2c2c2c;
+    text-decoration: none;
+  }
+`;
